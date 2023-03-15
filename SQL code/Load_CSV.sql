dfdae5bdad1709061ef0ff5430cc8e746fa19cde
danghuyhvpk <79554506+danghuyhvpk@users.noreply.github.com>
2023-03-15 15:38:23 +0700

    Add files via upload

---
+++ SQL code/Load_CSV.sql
@@ -0,0 +1,6 @@
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Orders.csv' 
+INTO TABLE orders 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
