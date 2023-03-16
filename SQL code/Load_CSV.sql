87e5f3c10ef89b7a2fad27b91a02b5dc6df00e01
danghuyhvpk <79554506+danghuyhvpk@users.noreply.github.com>
2023-03-17 02:43:50 +0700

    Delete SQL code directory

--- SQL code/Load_CSV.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Orders.csv' 
-INTO TABLE orders 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
